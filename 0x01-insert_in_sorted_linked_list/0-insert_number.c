546141dfdf9c6dc26a46782c13411ce47a2f352a
jdanielue <jdurregoe@gmail.com>
2022-08-11 07:30:35 -0500

    new code v2

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -1,71 +1,35 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
 
 /**
- * insert_node - insert node in sorted way
- * @head: -> to the list's first node
- * @number: int to be stored in the new node
- * Return: ->nde
+ * insert_node - inserts new node to linked list
+ * @head: head of singly linked list
+ * @number: value in singly linked list
+ *
+ * Return: the address of the new node, or NULL if it failed
  */
-listint_t *insert_node(listint_t **head, int number) {
-    listint_t *new_node = NULL;
-    listint_t *temp = NULL;
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new_node, *tmp1 = *head, *tmp2;
 
-    if (head == NULL) {
-        return (NULL);
-    }
-
-    if (*head == NULL) {
-        new_node = c_node(number);
-        *head = new_node;
-        return (new_node);
-    }
-    temp = *head;
-
-    if (number <= temp->n) {
-        new_node = c_node(number);
-        new_node->next = temp;
-        *head = new_node;
-        return (new_node);
-    }
-
-
-    while(temp->next) {
-        if (number <= temp->next->n) {
-            break;
-        }
-        temp = temp->next;
-    }
-
-
-    if (!temp->next) {
-        new_node = c_node(number);
-        temp->next = new_node;
-        return (new_node);
-    } 
-
-    new_node = c_node(number);
-    new_node->next = temp->next;
-    temp->next = new_node;
-
-    return (new_node);
-
-}
-
-/**
- * c_node - new listint_t node 
- * @number: int to be stored in the new node
- * Return: -> to new node
- */
-listint_t *c_node(int number) {
-    listint_t *new_node = NULL;
-
-    new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
+	if (!head)
 		return (NULL);
-    
-    new_node->n = number;
-    new_node->next=NULL;
+	new_node = malloc(sizeof(listint_t));
+	if (!new_node)
+		return (NULL);
 
-    return (new_node);
+	new_node->n = number;
+	if (!tmp1 || tmp1->n >= number)
+	{
+		new_node->next = tmp1, *head = new_node;
+		return (new_node);
+	}
+
+	tmp2 = tmp1->next;
+	while (tmp1 && tmp2 && (tmp2->n < number))
+		tmp1 = tmp1->next, tmp2 = tmp1->next;
+
+	tmp1->next = new_node, new_node->next = tmp2;
+	return (new_node);
 }
