c1d8630ffa28c952b15c6d4af92f2f8acf2ea342
jdanielue <jdurregoe@gmail.com>
2022-10-03 23:23:42 -0500

    second commit weith the validation

--- 0x07-linked_list_cycle/lists.h
+++ 0x07-linked_list_cycle/lists.h
@@ -20,6 +20,6 @@ typedef struct listint_s
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int num);
 void free_listint(listint_t *head);
-int check_cycle(listint_t *l);
+int check_cycle(listint_t *list);
 
 #endif 
