35d9eb8f5eacff2a15811babeefb55992ed8ede5
jdanielue <jdurregoe@gmail.com>
2023-03-23 07:52:21 -0500

    old project nqueen

---
+++ 0x0C-nqueens/0-nqueens.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+""" N QUEENS ALGORITHM WITH BACKTRACKING (RECURSION INSIDE LOOP) """
+import sys
+
+class NQueen:
+    """ Class resolve the issue"""
+
+    def __init__(self, n):
+        """ some data """
+        self.n = n
+        self.x = [0 for i in range(n + 1)]
+        self.res = []
+
+    def place(self, k, i):
+        """ verify if T or F"""
+
+        for m in range(1, k):
+
+            if self.x[m] == i or \
+               abs(self.x[m] - i) == abs(m - k):
+                return 0
+        return 1
+
+    def nQueen(self, k):
+        """ data from nQueen"""
+        for i in range(1, self.n + 1):
+            if self.place(k, i):
+                self.x[k] = i
+                if k == self.n:
+                    solution = []
+                    for i in range(1, self.n + 1):
+                        solution.append([i - 1, self.x[i] - 1])
+                    self.res.append(solution)
+                else:
+                    self.nQueen(k + 1)
+        return self.res
+
+
+
+if len(sys.argv) != 2:
+    print("Usage: nqueens N")
+    sys.exit(1)
+
+N = sys.argv[1]
+
+try:
+    N = int(N)
+except ValueError:
+    print("N must be a number")
+    sys.exit(1)
+
+if N < 4:
+    print("N must be at least 4")
+    sys.exit(1)
+
+queen = NQueen(N)
+res = queen.nQueen(1)
+
+for t in res:
+    print(t)
